2b9d6bff4f867f690f96e063ff08ac3a67dc420a
Rafael Rocha <rafaelrochasilva@gmail.com>
2016-02-04 09:35:52 -0200

    Add bill and tip calculator

--- simple_calculator/lib/simple_calculator/bill.ex
+++ simple_calculator/lib/simple_calculator/bill.ex
@@ -1,11 +1,8 @@
 defmodule SimpleCalculator.Bill do
-  @doc """
-   Calculates the bill tip
+  def total(tip_percentage, bill_total) do
+    tip(tip_percentage, bill_total) + bill_total
+  end
 
-   ## Example
-        iex> Bill.tip(15, 200)
-        30
-  """
   def tip(tip_percentage, bill_total) do
     bill_total * tip_percentage / 100
   end
--- simple_calculator/test/simple_calculator/bill_test.exs
+++ simple_calculator/test/simple_calculator/bill_test.exs
@@ -1,5 +1,11 @@
 defmodule SimpleCalculator.BillTest do
   use ExUnit.Case
 
-  doctest SimpleCalculator.BillTest
+  test "calculates the total of the bill with tip" do
+    assert 230.00 == SimpleCalculator.Bill.total(15, 200)
+  end
+
+  test "calculates a tip given a bill" do
+    assert 30.00 == SimpleCalculator.Bill.tip(15, 200)
+  end
 end
