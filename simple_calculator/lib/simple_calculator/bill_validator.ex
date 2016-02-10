b89ae9e20280781f5b9722a0ea2ca2585e69754d
Rafael Rocha <rafaelrochasilva@gmail.com>
2016-02-10 07:20:38 -0200

    Refactory bill validator functions

---
+++ simple_calculator/lib/simple_calculator/bill_validator.ex
@@ -0,0 +1,16 @@
+defmodule BillValidator do
+  def validates_type(value) do
+    case is_number(value) do
+      false -> "Only numbers are allowed"
+      true  -> value
+    end
+  end
+
+  def validates_numerically(value) when value <= 0 do
+    "Type only values greater than 0."
+  end
+
+  def validates_numerically(value) do
+    value
+  end
+end
