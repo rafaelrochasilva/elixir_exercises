0d8d1fa0fd805c513d79e45b99f101a84a61877e
Rafael Rocha <rafaelrochasilva@gmail.com>
2016-09-06 10:46:17 -0300

    Refactory code to make it more into a elixir way

--- simple_calculator/lib/simple_calculator/prompt.ex
+++ simple_calculator/lib/simple_calculator/prompt.ex
@@ -1,6 +1,6 @@
 defmodule SimpleCalculator.Prompt do
-  alias SimpleCalculator.BillValidator, as: BillValidator
-  alias SimpleCalculator.Bill, as: Bill
+  alias SimpleCalculator.BillValidator
+  alias SimpleCalculator.Bill
 
   def init do
     welcome_message
@@ -8,22 +8,28 @@ defmodule SimpleCalculator.Prompt do
   end
 
   defp welcome_message do
-    menu_bar
-    IO.puts "Hi, welcome to Simple Calculator."
-    IO.puts "We are going to help you to calculate the tips for a given bill :)"
+    """
+    ------------------------------------------------------------------
+    Hi, welcome to Simple Calculator.
+    We are going to help you to calculate the tips for a given bill :)
+    """
+      |> IO.puts
   end
 
   defp main_menu do
-    menu_bar
-    IO.puts "Main Menu:"
-    IO.puts "1. Generate bill"
-    IO.puts "2. Quit"
+    """
+    ------------------------------------------------------------------
+    Main Menu:
+    1. Generate bill
+    2. Quit:
+    """
+      |> IO.puts
+
     select_option
   end
 
   defp select_option do
-    menu_bar
-    option = IO.gets "Choose an option:\n"
+    option = "Choose an option:\n" |> IO.gets
     case option do
       "1\n" -> bill_menu()
       "2\n" -> quit()
@@ -32,12 +38,16 @@ defmodule SimpleCalculator.Prompt do
   end
 
   defp quit do
-    menu_bar
-    IO.puts "Good bye, have a nice day!"
+    """
+    ------------------------------------------------------------------
+    Good bye, have a nice day!
+    """
+      |> IO.puts
   end
 
   defp invalid_option do
-    IO.puts "**Please select only valid options"
+    "**Please select only valid options"
+      |> IO.puts
     main_menu
   end
 
@@ -45,34 +55,30 @@ defmodule SimpleCalculator.Prompt do
     bill = input_message("What is the bill?\n$")
     tip_percentage = input_message("What is the tip percentage?\n$")
 
-    total = Bill.total(tip_percentage, bill)
-    tip = Bill.tip(tip_percentage, bill)
+    total = SimpleCalculator.Bill.total(tip_percentage, bill)
+    tip = SimpleCalculator.Bill.tip(tip_percentage, bill)
 
-    IO.puts "The tip is $#{tip}"
-    IO.puts "The total is $#{total}"
-
-    main_menu
+    "The tip is $#{tip}"
+      |> IO.puts
+    "The total is $#{total}"
+      |> IO.puts
   end
 
   defp input_message(message) do
     input = IO.gets(message)
 
-    number = case BillValidator.validates_type(input) do
+    number = case SimpleCalculator.BillValidator.validates_type(input) do
       {:error, error_msg} ->
         IO.puts error_msg
         input_message(message)
       {:ok, value} -> value
     end
 
-    case BillValidator.validates_numerically(number) do
+    case SimpleCalculator.BillValidator.validates_numerically(number) do
       {:error, error_msg} ->
         IO.puts error_msg
         input_message(message)
       {:ok, value} -> value
     end
-  end
-
-  defp menu_bar do
-    IO.puts String.duplicate("-", 70)
   end
 end
