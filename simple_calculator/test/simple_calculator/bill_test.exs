03cd5202a16ad5a8d584f6f229284242ee1b925d
Rafael Rocha <rafaelrochasilva@gmail.com>
2016-09-08 10:16:20 -0300

    Include tests for Bill resume

--- simple_calculator/test/simple_calculator/bill_test.exs
+++ simple_calculator/test/simple_calculator/bill_test.exs
@@ -8,4 +8,8 @@ defmodule SimpleCalculator.BillTest do
   test "calculates a tip given a bill" do
     assert 30.00 == SimpleCalculator.Bill.tip(15, 200)
   end
+
+  test "calculates the total and tip and return as a tupple" do
+    assert { 230.00, 30.00 } == SimpleCalculator.Bill.resume(15, 200)
+  end
 end
